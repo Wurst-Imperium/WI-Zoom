a803efe0bbec456d7d0eace3ba698af476b089b9
Alexander01998 <contact.wurstimperium@gmail.com>
2019-10-29 22:22:22 +0200

    Initial Forge 1.12.2 setup (non-functional)

--- src/main/java/net/wurstclient/zoom/WiZoomInitializer.java
+++ src/main/java/net/wurstclient/zoom/WiZoomInitializer.java
@@ -7,24 +7,44 @@
  */
 package net.wurstclient.zoom;
 
-import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.Logger;
 
-public final class WiZoomInitializer implements ModInitializer
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = WiZoomInitializer.MODID,
+	name = WiZoomInitializer.NAME,
+	version = WiZoomInitializer.VERSION)
+public final class WiZoomInitializer
 {
 	private static boolean initialized;
 	
-	@Override
-	public void onInitialize()
+	public static final String MODID = "wi-zoom";
+	public static final String NAME = "WI Zoom";
+	public static final String VERSION = "1.0";
+	
+	private static Logger logger;
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event)
 	{
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		logger = event.getModLog();
+	}
+	
+	@EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+		// some example code
+		logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
 		
 		if(initialized)
 			throw new RuntimeException(
 				"WiZoomInitializer.onInitialize() ran twice!");
 		
-		WiZoom.INSTANCE.initialize();
+		// WiZoom.INSTANCE.initialize();
 		initialized = true;
 	}
 }
