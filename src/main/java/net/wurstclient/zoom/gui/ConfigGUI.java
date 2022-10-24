e8332d867f1bdfd0d472f7a1c7ed356f145b0c92
The Changer <thechanger412@gmail.com>
2022-10-24 17:30:10 -0500

    tweak the gui and use google translate on the other languages

--- src/main/java/net/wurstclient/zoom/gui/ConfigGUI.java
+++ src/main/java/net/wurstclient/zoom/gui/ConfigGUI.java
@@ -6,7 +6,6 @@ import io.github.cottonmc.cotton.gui.widget.data.HorizontalAlignment;
 import io.github.cottonmc.cotton.gui.widget.data.Insets;
 import io.github.cottonmc.cotton.gui.widget.data.Axis;
 import io.github.cottonmc.cotton.gui.widget.data.VerticalAlignment;
-import net.minecraft.SaveVersion;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
@@ -16,26 +15,26 @@ public class ConfigGUI extends LightweightGuiDescription {
     public ConfigGUI(Screen previous) {
         WGridPanel root = new WGridPanel();
         setRootPanel(root);
-        root.setSize(360, 90);
+        root.setSize(340, 90);
         root.setInsets(Insets.NONE);
 
-        WLabel defaultLevelText = new WLabel(Text.of("Default Level: "), 0x000000);
+        WLabel defaultLevelText = new WLabel(Text.translatable("gui.wi_zoom.label"), 0x000000);
         defaultLevelText.setVerticalAlignment(VerticalAlignment.CENTER);
         root.add(defaultLevelText, 1, 1, 5, 1);
         
         WiWLabeledSlider defaultLevelLabelSlider = new WiWLabeledSlider(1, 50, Axis.HORIZONTAL);
         defaultLevelLabelSlider.setValue((int) WiZoom.INSTANCE.getDefaultLevel());
         defaultLevelLabelSlider.setLabel(Text.of(String.valueOf((int) WiZoom.INSTANCE.getDefaultLevel())));
-        root.add(defaultLevelLabelSlider, 6, 1, 12, 1);
+        root.add(defaultLevelLabelSlider, 10, 1, 8, 1);
         
-        WButton saveButton = new WButton(Text.of("Save & Exit"));
+        WButton saveButton = new WButton(Text.translatable("gui.wi_zoom.button"));
         saveButton.setOnClick(() -> {
             WiZoom.INSTANCE.setDefaultLevel(defaultLevelLabelSlider.getValue());
             WiZoom.saveConfig();
             MinecraftClient.getInstance().setScreen(previous);
         });
         saveButton.setAlignment(HorizontalAlignment.CENTER);
-        root.add(saveButton, 8, 3, 5, 2);
+        root.add(saveButton, 5, 3, 10, 2);
 
         root.validate(this);
     }
