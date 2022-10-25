7dd59191766dc2a03434f44937f8366222f88cfb
The Changer <thechanger412@gmail.com>
2022-10-24 22:47:22 -0500

    made the gui manually change size based on the language

--- src/main/java/net/wurstclient/zoom/gui/ConfigGUI.java
+++ src/main/java/net/wurstclient/zoom/gui/ConfigGUI.java
@@ -15,26 +15,64 @@ public class ConfigGUI extends LightweightGuiDescription {
     public ConfigGUI(Screen previous) {
         WGridPanel root = new WGridPanel();
         setRootPanel(root);
-        root.setSize(340, 90);
+        root.setSize(360, 100);
         root.setInsets(Insets.NONE);
 
         WLabel defaultLevelText = new WLabel(Text.translatable("gui.wi_zoom.label"), 0x000000);
         defaultLevelText.setVerticalAlignment(VerticalAlignment.CENTER);
-        root.add(defaultLevelText, 1, 1, 5, 1);
         
         WiWLabeledSlider defaultLevelLabelSlider = new WiWLabeledSlider(1, 50, Axis.HORIZONTAL);
         defaultLevelLabelSlider.setValue((int) WiZoom.INSTANCE.getDefaultLevel());
         defaultLevelLabelSlider.setLabel(Text.of(String.valueOf((int) WiZoom.INSTANCE.getDefaultLevel())));
-        root.add(defaultLevelLabelSlider, 10, 1, 8, 1);
         
         WButton saveButton = new WButton(Text.translatable("gui.wi_zoom.button"));
+        saveButton.setAlignment(HorizontalAlignment.CENTER);
         saveButton.setOnClick(() -> {
             WiZoom.INSTANCE.setDefaultLevel(defaultLevelLabelSlider.getValue());
             WiZoom.saveConfig();
             MinecraftClient.getInstance().setScreen(previous);
         });
-        saveButton.setAlignment(HorizontalAlignment.CENTER);
-        root.add(saveButton, 5, 3, 10, 2);
+
+        System.out.println(MinecraftClient.getInstance().getLanguageManager().getLanguage().getName());
+        System.out.println(MinecraftClient.getInstance().getLanguageManager().getLanguage().getCode());
+        System.out.println(MinecraftClient.getInstance().getLanguageManager().getLanguage().getRegion());
+//        root.add(defaultLevelText, 1, 1, 1 ,1);
+//        root.add(defaultLevelLabelSlider, 1, 1, 1,1);
+//        root.add(saveButton, 1, 1, 1 ,1);
+
+        if (MinecraftClient.getInstance().getLanguageManager().getLanguage().getCode().equals("fr_fr")) {
+            root.add(defaultLevelText, 1, 1, 5 ,1);
+            root.add(defaultLevelLabelSlider, 9, 1, 10 ,1);
+            root.add(saveButton, 6, 3, 8 ,4);
+        } else if (MinecraftClient.getInstance().getLanguageManager().getLanguage().getCode().equals("zh_hk")) {
+            root.add(defaultLevelText, 1, 1, 5 ,1);
+            root.add(defaultLevelLabelSlider, 5, 1, 14,1);
+            root.add(saveButton, 7, 3, 6 ,4);
+        } else if (MinecraftClient.getInstance().getLanguageManager().getLanguage().getCode().equals("zh_cn")) {
+            root.add(defaultLevelText, 1, 1, 5 ,1);
+            root.add(defaultLevelLabelSlider, 5, 1, 14,1);
+            root.add(saveButton, 7, 3, 6 ,4);
+        } else if (MinecraftClient.getInstance().getLanguageManager().getLanguage().getCode().equals("zh_tw")) {
+            root.add(defaultLevelText, 1, 1, 5 ,1);
+            root.add(defaultLevelLabelSlider, 5, 1, 14,1);
+            root.add(saveButton, 7, 3, 6 ,4);
+        } else if (MinecraftClient.getInstance().getLanguageManager().getLanguage().getCode().equals("lzh")) {
+            root.add(defaultLevelText, 1, 1, 5 ,1);
+            root.add(defaultLevelLabelSlider, 5, 1, 14,1);
+            root.add(saveButton, 7, 3, 6 ,4);
+        } else if (MinecraftClient.getInstance().getLanguageManager().getLanguage().getCode().equals("ru_ru")) {
+            root.add(defaultLevelText, 1, 1, 5 ,1);
+            root.add(defaultLevelLabelSlider, 13, 1, 6,1);
+            root.add(saveButton, 7, 3, 6 ,4);
+        } else if (MinecraftClient.getInstance().getLanguageManager().getLanguage().getCode().equals("et_ee")) {
+            root.add(defaultLevelText, 1, 1, 5 ,1);
+            root.add(defaultLevelLabelSlider, 7, 1, 12,1);
+            root.add(saveButton, 7, 3, 6 ,4);
+        } else {
+            root.add(defaultLevelText, 2, 1, 5 ,1);
+            root.add(defaultLevelLabelSlider, 8, 1, 11,1);
+            root.add(saveButton, 7, 3, 6 ,4);
+        }
 
         root.validate(this);
     }
